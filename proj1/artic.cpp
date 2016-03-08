4fdf4b32b59017c411571fca50c717a0faf81dab
xtrm0 <afonso.tfaria21@gmail.com>
2016-03-08 23:43:29 +0000

    More clean up

--- proj1/artic.cpp
+++ proj1/artic.cpp
@@ -12,17 +12,22 @@ vector<int> pi; //parent
 vector<bool> AP; //É Articulation Point?
 
 /* dfs: Corre o dfs-visit do algoritmo de Tarjan
- * Devolve: O número de filhos na àrvore DFS */
+ * Devolve: O número de filhos na àrvore DFS
+
+ * O código é baseado no algoritmo dfs do ch22.pdf das aulas teóricas,
+ adicionando a parte referente à procura de filhos, e usando o vetor d para
+ guardar informação sobre a cor (havendo apenas as cores branco e preto).
+ */
 int dfs(int u) {
   int ret=0;
-  low[u] = d[u] = ts;
+  low[u] = d[u] = ts; //define o start time do nó
   ++ts;
   for (vector<int>::iterator it = G[u].begin(); it != G[u].end(); it++) {
     int v = *it;
-    if (d[v]==0) {
-      ret++;
-      pi[v] = u;
-      dfs(v);
+    if (d[v] == 0) { //Se ainda não foi visitado:
+      ret++; //aumenta o numero de filhos na arvore dfs do no u
+      pi[v] = u; //define o pai de v como sendo u
+      dfs(v); //corre o dfs-visit em v
       if (low[v] >= d[u]) //Condição suficiente para ser AP para nós que não sejam a raíz
         AP[u] = true;
       low[u] = min(low[u], low[v]);
@@ -55,7 +60,7 @@ int main() {
   /* Chama a DFS: */
   for (int u=0; u<N; u++)
     if (d[u] == 0)
-      AP[u] = dfs(u) > 1; //Condição de aticulation point para raízes de àrvore
+      AP[u] = dfs(u) > 1; //Condição de articulation point para raízes de àrvore
 
   /* Imprime os resultados: */
   int apCount = 0;
@@ -63,7 +68,7 @@ int main() {
   for (int u=0; u<N; u++) {
     if (AP[u]) {
       apCount++;
-      if (first==-1) first = u;
+      if (first == -1) first = u;
       last = u;
     }
   }
